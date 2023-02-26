e5141e17fcf3681855a816bd24f797b266e93396
vithumvindeepa <vindeepavithum@gmail.com>
2023-02-26 20:23:31 +0530

    implements methods

--- src/main/java/lk/ijse/dep/service/BoardImpl.java
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -1,6 +1,6 @@
 package lk.ijse.dep.service;
 
-public class BoardImpl {
+public class BoardImpl implements Board{
     private static final int NUM_OF_COLS = 6;
     private static final int NUM_OF_ROWS = 5;
     private final BoardUI boardUI;
@@ -10,5 +10,35 @@ public class BoardImpl {
     public BoardImpl(BoardUI boardUI) {
         this.boardUI = boardUI;
         this.pieces = new Piece[NUM_OF_ROWS][NUM_OF_COLS];
+    }
+
+    @Override
+    public BoardUI getBoardUI() {
+        return null;
+    }
+
+    @Override
+    public int findNextAvailableSpot(int col) {
+        return 0;
+    }
+
+    @Override
+    public boolean isLegalMove(int col) {
+        return false;
+    }
+
+    @Override
+    public boolean existLegalMoves() {
+        return false;
+    }
+
+    @Override
+    public void updateMove(int col, Piece move) {
+
+    }
+
+    @Override
+    public Winner findWinner() {
+        return null;
     }
 }
