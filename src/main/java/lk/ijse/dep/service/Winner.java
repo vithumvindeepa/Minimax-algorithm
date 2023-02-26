4a055ebf3ccdcf0d092a2bd96d439190e17793d1
vithumvindeepa <vindeepavithum@gmail.com>
2023-02-26 20:35:47 +0530

    creat getter setter methods

--- src/main/java/lk/ijse/dep/service/Winner.java
+++ src/main/java/lk/ijse/dep/service/Winner.java
@@ -1,11 +1,87 @@
 package lk.ijse.dep.service;
 
 public class Winner {
+    private Piece winningPiece;
     private static final int NUM_OF_COLS = 8;
-    int call;
-    int row1;
-    int cal2;
-    int row2;
+    private static final int NUM_OF_ROWS = 8;
+    private Piece[][] pieces;
+    private int col1;
+    private int row1;
+    private int col2;
 
+
+
+    private int row2;
+
+    public Winner(Piece winningPiece) {
+        this.setPieces(new Piece[getNumOfCols()][getNumOfRows()]);
+    }
+
+    public static int getNumOfCols() {
+        return NUM_OF_COLS;
+    }
+
+    public static int getNumOfRows() {
+        return NUM_OF_ROWS;
+    }
+
+    public Piece getWinningPiece() {
+        return winningPiece;
+    }
+
+    public void setWinningPiece(Piece winningPiece) {
+        this.winningPiece = winningPiece;
+    }
+
+    public Piece[][] getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(Piece[][] pieces) {
+        this.pieces = pieces;
+    }
+
+    public int getCol1() {
+        return col1;
+    }
+
+    public void setCol1(int col1) {
+        this.col1 = col1;
+    }
+
+    public int getRow1() {
+        return row1;
+    }
+
+    public void setRow1(int row1) {
+        this.row1 = row1;
+    }
+
+    public int getCol2() {
+        return col2;
+    }
+
+    public void setCol2(int col2) {
+        this.col2 = col2;
+    }
+
+    public int getRow2() {
+        return row2;
+    }
+
+    public void setRow2(int row2) {
+        this.row2 = row2;
+    }
+
+    @Override
+    public String toString() {
+        return "Winner{" +
+                "winningPiece=" + winningPiece +
+                ", col1=" + col1 +
+                ", row1=" + row1 +
+                ", col2=" + col2 +
+                ", row2=" + row2 +
+                '}';
+    }
 
 }
