3814e5b2db1390138232478f8d4f0fa91c587517
vithumvindeepa <vindeepavithum@gmail.com>
2023-02-26 20:15:58 +0530

    creat arrays

--- src/main/java/lk/ijse/dep/service/BoardImpl.java
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -1,4 +1,14 @@
 package lk.ijse.dep.service;
 
 public class BoardImpl {
+    private static final int NUM_OF_COLS = 6;
+    private static final int NUM_OF_ROWS = 5;
+    private final BoardUI boardUI;
+    private final Piece[][] pieces;
+
+
+    public BoardImpl(BoardUI boardUI) {
+        this.boardUI = boardUI;
+        this.pieces = new Piece[NUM_OF_ROWS][NUM_OF_COLS];
+    }
 }
